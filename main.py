0c10cba41ea92b91648bcff1d5e6d498e9d9a038
MAJHCC <info@majhcc.xyz>
2021-11-30 12:46:19 +0400

    first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+from flask import request, Flask, jsonify, render_template, redirect
+app = Flask(__name__)
+
+@app.route('/')
+def main_route():
+    return redirect('/get_my_ip')
+
+@app.route("/get_my_ip", methods=["GET"])
+def get_my_ip():
+    return jsonify({"ip" : request.remote_addr}), 200
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=80, debug=True)
